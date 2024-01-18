1127bab32b7c9bb845bbbb24830ed9311d6c35fd
atef0212 <che.6178a@gmail.com>
2024-01-18 15:47:01 +0100

    Navigation is finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import './App.css'
+import Layout from './components/Layout'
+import Carts from './components/Carts'
+import Products from './components/Products'
 
 function App() {
-
+  const router=createBrowserRouter(createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='/carts' element={<Carts/>}/>
+      <Route path='/products' element={<Products/>}/>
+    </Route>
+  ))
   return (
     <>
-      <h1 className='w-8 text-red-950'>hello</h1>
+     <RouterProvider router={router}/>
     </>
   )
 }
-
 export default App
---
+++ src/components/Headers.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom'
+
+function Headers() {
+  return (
+   <>
+    <nav>
+        <ul>
+        <li> <Link to="/">Home</Link></li>
+           
+        </ul>
+        <ul>
+        <li> <Link to="/carts">Carts</Link></li>
+           
+        </ul>
+        <ul>
+           <li> <Link to="/products">Products</Link></li>
+        </ul>
+    </nav>
+   </>
+  )
+}
+
+export default Headers
