06e098722879a9990d1aa4355c9af3081f5b8192
atef0212 <che.6178a@gmail.com>
2024-01-23 09:37:05 +0100

    useReducer, addBtn, finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,64 @@
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements , Link} from 'react-router-dom'
 import './App.css'
 import Layout from './components/Layout'
 import Carts from './components/Carts'
 import Products from './components/Products'
-
+import Home from './components/Home'
+import { initState, reducer } from './Context-Api-Reducer/Usereducer'
+import { useEffect, useReducer, useState } from 'react'
+import ShopData from './Context-Api-Reducer/Context'
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((respond) => respond.json())
+      .then((dat) => setData(dat)).catch ((error)=> console.error("Error fetching data:", error));
+   
+  }, []);
+
+
+
+
+
+const [state, dispatch]=useReducer(reducer, initState)
+
+function addToBasket  (product)  {
+  const updateBasket =[...state.products, product]
+
+  dispatch({ type: "AddToBasket", payload: updateBasket });
+}
+
+
+
+
+
+
+
+
+const values={
+  data,
+  addToBasket,
+  products:state.products,
+  totalPrice:state.totalPrice,
+
+}
+
   const router=createBrowserRouter(createRoutesFromElements(
     <Route path='/' element={<Layout/>}>
-      <Route path='/carts' element={<Carts/>}/>
-      <Route path='/products' element={<Products/>}/>
+    <Route path='/home' element={<Home/>}/>
+    <Route path='/carts' element={<Carts/>}/>
+    <Route path='/products/:category?' element={<Products/>}/>
     </Route>
   ))
   return (
     <>
-     <RouterProvider router={router}/>
+<ShopData.Provider value={values}>
+
+<RouterProvider router={router}/>
+</ShopData.Provider>
+   
     </>
   )
 }
