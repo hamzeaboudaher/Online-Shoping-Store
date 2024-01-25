44ea8ff11bbfa098a1215278bb9e7cccec275795
atef0212 <che.6178a@gmail.com>
2024-01-25 14:55:30 +0100

    ..

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,24 +1,12 @@
-<<<<<<< HEAD
-import Footer from './footer/Footer'
-import Headers from './Headers'
-import { Outlet } from 'react-router-dom'
-
-function Layout() {
-  return (
-    <>
-    <Headers/>
-    <Footer/>
-        <Outlet/>
-        
-=======
 import Headers from "./Headers";
 import { Outlet } from "react-router-dom";
+import Footer from "./footer/Footer";
 function Layout() {
   return (
     <>
       <Headers />
+      <Footer/>
       <Outlet />
->>>>>>> origin/usebranch
     </>
   );
 }
